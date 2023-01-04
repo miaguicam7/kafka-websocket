569e361177c1e85c926b71c584a874cd92331878
Miguel Ángel Aguilar <maaguilar@profile.es>
2023-01-04 10:23:13 +0100

    añadido session id

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.stereotype.Component;
@@ -25,7 +26,6 @@ import reactor.kafka.receiver.ReceiverOffset;
 import reactor.kafka.receiver.ReceiverOptions;
 import reactor.kafka.receiver.ReceiverRecord;
 
-import java.time.Instant;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -63,15 +63,19 @@ class ThymeleafController {
 }
 
 @Controller
+@Slf4j
 class WebSocketBroadcastController {
     @Autowired
     KafkaConsumer kafkaConsumer;
 
     @MessageMapping("/broadcast")
     @SendTo("/topic/messages")
-    public void send(String str) {
+    public void send(SimpMessageHeaderAccessor headerAccessor, String str) {
         if (str.equals("{\"from\":\"server\",\"text\":\"connected to server\"}")) {
-            kafkaConsumer.consumeMessages("demo-topic");
+            // Obtén la sesión a partir del encabezado del mensaje
+            String sessionId = headerAccessor.getSessionId();
+            log.info("session id: " + sessionId);
+            kafkaConsumer.consumeMessages("demo-topic", sessionId);
         }
     }
 }
@@ -81,37 +85,44 @@ class WebSocketBroadcastController {
 class KafkaConsumer {
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
-    private final ReceiverOptions<Integer, String> receiverOptions;
 
-    public KafkaConsumer() {
+    //Listar los topicos que correspondan a un regex
+    // Contador de ingestas durante el día
+    //host izquierdo front derecha
+    //aparece y desaparece
+    //Inicializar los elementos de debajo en la cantidad ingestada
+    // un origen destino
 
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
-        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "sample-consumer");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "sample-group");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        receiverOptions = ReceiverOptions.create(props);
-    }
+    public void consumeMessages(String topic, String sessionId) {
 
-    public void consumeMessages(String topic) {
-        ReceiverOptions<Integer, String> options = receiverOptions.subscription(Collections.singleton(topic))
-                .addAssignListener(partitions -> log.debug("onPartitionsAssigned {}", partitions))
-                .addRevokeListener(partitions -> log.debug("onPartitionsRevoked {}", partitions));
+        ReceiverOptions<Integer, String> receiverOptions = obtainProperties(topic, sessionId);
 
-        Flux<ReceiverRecord<Integer, String>> kafkaFlux = KafkaReceiver.create(options).receive();
+        Flux<ReceiverRecord<Integer, String>> kafkaFlux = KafkaReceiver.create(receiverOptions).receive();
         kafkaFlux.subscribe(record -> {
             ReceiverOffset offset = record.receiverOffset();
-            Instant timestamp = Instant.ofEpochMilli(record.timestamp());
             log.info("Received message: topic-partition={} offset={}  key={} value={}",
                     offset.topicPartition(),
                     offset.offset(),
                     record.key(),
                     record.value());
-            messagingTemplate.convertAndSend("/topic/broadcast", record.value());
+            messagingTemplate.convertAndSend("/topic/broadcast", String.format("session id %s value %s", sessionId, record.value()));
             offset.acknowledge();
         });
+    }
+
+    private ReceiverOptions<Integer, String> obtainProperties(String topic, String groupId) {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
+        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "sample-consumer");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        // props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        ReceiverOptions<Integer, String> receiverOptions = ReceiverOptions.create(props);
+
+        return receiverOptions.subscription(Collections.singleton(topic))
+                .addAssignListener(partitions -> log.debug("onPartitionsAssigned {}", partitions))
+                .addRevokeListener(partitions -> log.debug("onPartitionsRevoked {}", partitions));
     }
 }
 
